9bbbb5b7c8f075da88b81725b8595d15cf87284e
Camilly <camilydetani@gmail.com>
2026-05-27 10:05:03 -0300

    resenha suprema real 3.0

--- app.py
+++ app.py
@@ -1,20 +1,42 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
+import mysql.connector
 
 app = Flask(__name__)
 
+
+conexao = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="login"
+)
+
 @app.route('/')
 def home():
-    # Variáveis que serão enviadas para o template
-    
-    # Renderiza o template e passa as variáveis
     return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+
+    email = request.form['email']
+    senha = request.form['senha']
+
+    cursor = conexao.cursor()
+
+    comando = "SELECT * FROM Usuarios WHERE email=%s AND senha=%s"
+    valores = (email, senha)
+
+    cursor.execute(comando, valores)
+
+    usuario = cursor.fetchone()
+
+    if usuario:
+        return redirect('/dps')
+
+    return "Email ou senha incorretos"
 
 @app.route('/dps')
 def pag():
-    # Variáveis que serão enviadas para o template
-    
-    # Renderiza o template e passa as variáveis
     return render_template('dps.html')
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+app.run(debug=True)
