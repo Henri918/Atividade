8bca180cd8c9f6145d642997da06251770e917ca
Camilly <camilydetani@gmail.com>
2026-05-27 08:06:06 -0300

    Versao 1.2

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    # Variáveis que serão enviadas para o template
+    
+    # Renderiza o template e passa as variáveis
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, host=0.0.0.0)
